327ee3a82d5b2898a45080937c619b365106db3f
Kirin Patel <kirinpatel@gmail.com>
2020-06-20 18:15:01 -0400

    Started using flex in DealScreen

--- src/screens/DealScreen.tsx
+++ src/screens/DealScreen.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
-import { Deal } from '../lib/types'
+import { Deal, DealItem } from '../lib/types'
 import ImageCarousel from '../components/DealImageCarousel';
 
 type RouteParams = {
@@ -24,6 +24,18 @@ export default class extends Component<Props, State> {
     };
   }
 
+  _getPrice = (): string => {
+    const { deal } = this.state;
+
+    const prices = deal.items.map((item: DealItem) => item.price);
+    const maxPrice = Math.max(...prices);
+    const minPrice = Math.min(...prices);
+    if (maxPrice !== minPrice) {
+      return `$${minPrice} - $${maxPrice}`;
+    }
+    return `$${minPrice}`;
+  };
+
   render(): JSX.Element {
     const { deal } = this.state;
 
@@ -34,16 +46,23 @@ export default class extends Component<Props, State> {
     return <>
       <StatusBar barStyle={barStyle} />
       <View style={[styles.container, { backgroundColor }]}>
-        <SafeAreaView>
+        <SafeAreaView style={styles.fill}>
           <ImageCarousel photos={deal.photos} theme={deal.theme} />
-          <Text
-            numberOfLines={2}
-            adjustsFontSizeToFit={true}
-            minimumFontScale={0.5}
-            style={[styles.title, { color: accentColor }]}
-          >
-            {deal.title}
-          </Text>
+          <View style={styles.itemContainer}>
+            <Text
+              numberOfLines={2}
+              adjustsFontSizeToFit={true}
+              minimumFontScale={0.5}
+              style={[styles.item, styles.title, { color: accentColor }]}
+            >
+              {deal.title}
+            </Text>
+            <View style={[styles.item, styles.detailsContainer]}>
+              <Text style={[styles.price, { color: accentColor }]}>
+                {this._getPrice()}
+              </Text>
+            </View>
+          </View>
         </SafeAreaView>
       </View>
     </>;
@@ -51,12 +70,35 @@ export default class extends Component<Props, State> {
 };
 
 const styles = StyleSheet.create({
+  fill: {
+    flex: 1,
+  },
   container: {
     width: '100%',
     height: '100%',
   },
+  itemContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
+  item: {
+    marginTop: 8,
+    marginBottom: 8,
+    marginLeft: 16,
+    marginRight: 16,
+  },
   title: {
     fontSize: 36,
-    margin: 16,
+    fontWeight: '500'
+  },
+  price: {
+    fontSize: 20,
+    flexGrow: 1
+  },
+  detailsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
   }
 });
